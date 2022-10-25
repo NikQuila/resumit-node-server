d2f081e6170eed6e29b6a2503d896375641d022d
NikQuila <nicopirozzi1@gmail.com>
2022-10-25 12:29:51 -0300

    sip

--- Controllers/notificationWebhooks.js
+++ Controllers/notificationWebhooks.js
@@ -8,7 +8,7 @@ const mandarMail = require("../mailgun/mailgun");
 const axios = require("axios");
 
 const PagarProducto = async (req, res) => {
-  if (req.body.id) {
+  if (req.body.action === "payment.created") {
     const payment = await axios.get(
       `https://api.mercadopago.com/v1/payments/${req.body.data.id}`,
       {
@@ -18,33 +18,36 @@ const PagarProducto = async (req, res) => {
         },
       }
     );
-    const {
-      uid,
-      resumit_id,
-      resumit_user_id,
-      email,
-      unit_price,
-      email_vendedor,
-      name_comprador,
-      title,
-    } = payment.data.metadata;
-    const unit_with_taxes = unit_price * 0.77;
-    downloadedFileAndSaveInUserFS(uid, resumit_id);
-    downloadedFileAndSaveInResumitFS(
-      uid,
-      email,
-      resumit_id,
-      resumit_user_id,
-      unit_with_taxes
-    );
-    await mandarMail(
-      process.env.KEY_MAILGUN,
-      email_vendedor,
-      name_comprador,
-      title,
-      unit_price
-    );
+    if (payment.data.status === "approved") {
+      const {
+        uid,
+        resumit_id,
+        resumit_user_id,
+        email,
+        unit_price,
+        email_vendedor,
+        name_comprador,
+        title,
+      } = payment.data.metadata;
+      const unit_with_taxes = unit_price * 0.77;
+      downloadedFileAndSaveInUserFS(uid, resumit_id);
+      downloadedFileAndSaveInResumitFS(
+        uid,
+        email,
+        resumit_id,
+        resumit_user_id,
+        unit_with_taxes
+      );
+      await mandarMail(
+        process.env.KEY_MAILGUN,
+        email_vendedor,
+        name_comprador,
+        title,
+        unit_price
+      );
+    }
   }
+
   res.status(200).send("Ok");
 };
 
